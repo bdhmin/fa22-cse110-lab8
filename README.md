6928783020ace6d7234269a755c7ba4a2815b392
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2022-11-17 08:20:47 -0800

    Wrote 20/20 populated and Add to Cart tests

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Lab 8 - Starter
+
+### 1. Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+
+I would fit my automated tests in a GitHub action that gets triggered after a push. This ensures that code is continuously being checked to be cleared of errors. Instead of testing after the entier development cycle is complete like in answer 3, incremental testing makes bugifxing much easier and helps the developers stay in the right track through the entire dev process. I would not be opposed to answer 2 since the testing is still being done before the push. This makes sure the tests pass before pushing any code, but I am assuming all features are in branches that do not affect the main version. Testing with GitHub actions poses an advantage that running tests asynchronously can improve the efficiency of development since the developer does not necessarily wait for the test to complete before pushing. Although pushing clean code is always an advantage.
+
+### 2. Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+
+No. end to end testing is used for the correct user flow and routing of the page; it is not focused on correctness of functions—that is the job for unit tests.
+
+### 3.
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -23,22 +23,19 @@ describe('Basic user flow for Website', () => {
     let data, plainValue;
     // Query select all of the <product-item> elements
     const prodItems = await page.$$('product-item');
-    console.log(`Checking product item 1/${prodItems.length}`);
-    // Grab the .data property of <product-items> to grab all of the json data stored inside
-    data = await prodItems[0].getProperty('data');
-    // Convert that property to JSON
-    plainValue = await data.jsonValue();
-    // Make sure the title, price, and image are populated in the JSON
-    if (plainValue.title.length == 0) { allArePopulated = false; }
-    if (plainValue.price.length == 0) { allArePopulated = false; }
-    if (plainValue.image.length == 0) { allArePopulated = false; }
+    for (let [index, prodItem] of prodItems.entries()) {
+      console.log(`Checking product item ${index+1}/${prodItems.length}`);
+      // Grab the .data property of <product-items> to grab all of the json data stored inside
+      data = await prodItem.getProperty('data');
+      // Convert that property to JSON
+      plainValue = await data.jsonValue();
+      // Make sure the title, price, and image are populated in the JSON
+      if (plainValue.title.length == 0) { allArePopulated = false; }
+      if (plainValue.price.length == 0) { allArePopulated = false; }
+      if (plainValue.image.length == 0) { allArePopulated = false; }
+    }
     // Expect allArePopulated to still be true
     expect(allArePopulated).toBe(true);
-
-    // TODO - Step 1
-    // Right now this function is only checking the first <product-item> it found, make it so that
-    // it checks every <product-item> it found
-
   }, 10000);
 
   // Check to make sure that when you click "Add to Cart" on the first <product-item> that
@@ -47,9 +44,16 @@ describe('Basic user flow for Website', () => {
     console.log('Checking the "Add to Cart" button...');
     // TODO - Step 2
     // Query a <product-item> element using puppeteer ( checkout page.$() and page.$$() in the docs )
+    const prodItem = await page.$('product-item');
     // Grab the shadowRoot of that element (it's a property), then query a button from that shadowRoot.
+    const prodShadowRoot = await prodItem.getProperty('shadowRoot');
+    const button = await prodShadowRoot.$('button');
     // Once you have the button, you can click it and check the innerText property of the button.
+    await button.click();
     // Once you have the innerText property, use innerText['_remoteObject'].value to get the text value of it
+    const buttonProperty = await button.getProperty('innerHTML');
+    const innerHTML = await buttonProperty.jsonValue();
+    expect(innerHTML).toBe('Remove from Cart');
   }, 2500);
 
   // Check to make sure that after clicking "Add to Cart" on every <product-item> that the Cart
